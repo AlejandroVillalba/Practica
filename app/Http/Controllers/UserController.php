d17dfe6aae00ce9073217b754e6bcd307cffe8f8
AlejandroVillalba <alejandrovillalba007@gmail.com>
2020-06-22 17:48:10 -0400

    paginacion tabla users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         $query = trim($request->get('search'));
         $users = User::where('name', 'LIKE', '%' . $query . '%')
           ->orderBy('id', 'asc')
-          ->get();
+          ->paginate(1);
 
         return view('usuarios.index', ['users' => $users, 'search'=> $query]);
       }
