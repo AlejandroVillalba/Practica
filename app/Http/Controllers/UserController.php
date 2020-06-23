304e934322ead92edb6968666668ca9e712dd8ef
AlejandroVillalba <alejandrovillalba007@gmail.com>
2020-06-23 11:08:02 -0400

    Vista login segun diseño

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         $query = trim($request->get('search'));
         $users = User::where('name', 'LIKE', '%' . $query . '%')
           ->orderBy('id', 'asc')
-          ->paginate(1);
+          ->paginate(5);
 
         return view('usuarios.index', ['users' => $users, 'search'=> $query]);
       }
